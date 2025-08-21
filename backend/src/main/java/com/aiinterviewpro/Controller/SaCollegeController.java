506c046275b629b81d0130cb040043233a06831d
Aruna R <ravithilagavathi922@gmail.com>
2025-08-21 13:59:45 +0530

    crossorgin

--- backend/src/main/java/com/aiinterviewpro/Controller/SaCollegeController.java
+++ backend/src/main/java/com/aiinterviewpro/Controller/SaCollegeController.java
@@ -4,10 +4,11 @@ import com.aiinterviewpro.DTO.SaCollegeSummaryDto;
 import com.aiinterviewpro.Service.SaCollegeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/sa/super/admin")
 public class SaCollegeController {
--- backend/src/main/java/com/aiinterviewpro/Controller/SaRequestColController.java
+++ backend/src/main/java/com/aiinterviewpro/Controller/SaRequestColController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/sa/request")
 public class SaRequestColController {
