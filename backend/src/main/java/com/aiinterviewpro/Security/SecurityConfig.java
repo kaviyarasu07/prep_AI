bd9a9bead19b48347f693d6f1e81f58968aa0cd0
Tamilnila <tamilnilag@gmail.com>
2025-08-25 16:45:58 +0530

    sec

--- backend/src/main/java/com/aiinterviewpro/Security/SecurityConfig.java
+++ backend/src/main/java/com/aiinterviewpro/Security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/test/**").permitAll()
+                        .requestMatchers("/test/**").permitAll() 
                         .requestMatchers("/auth/register", "/auth/login", "/auth/forgot-password", "/auth/refresh-token").permitAll()
                         .requestMatchers("/auth/reset-password").authenticated()
                         .requestMatchers("/auth/**","/ca/dashBoard/**","/ca/department/**","ca/search/**").permitAll()
