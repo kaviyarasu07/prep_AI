68d21cad059d0d46611a43c9eb71705142cd0315
RishiKumaar-23 <rishikumaar.nkl@gmail.com>
2025-08-25 16:30:40 +0530

    final commit 2

--- backend/src/main/java/com/aiinterviewpro/Security/SecurityConfig.java
+++ backend/src/main/java/com/aiinterviewpro/Security/SecurityConfig.java
@@ -31,10 +31,10 @@ public class SecurityConfig {
                 .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("test/**","/request/**").permitAll()
-                        .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/forgot-password", "/api/auth/refresh-token").permitAll()
-                        .requestMatchers("/api/auth/reset-password").authenticated() //
-                        .requestMatchers("/api/sa/**").hasRole("SUPER_ADMIN")
-                        .requestMatchers("/api/ca/**").hasRole("COLLEGE_ADMIN")
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/forgot-password", "/auth/refresh-token").permitAll()
+                        .requestMatchers("/auth/reset-password").authenticated() //
+                        .requestMatchers("/sa/**").hasRole("SUPER_ADMIN")
+                        .requestMatchers("/ca/**").hasRole("COLLEGE_ADMIN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
