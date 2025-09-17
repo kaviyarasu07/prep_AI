4c92688b80822038fd86b16a663fb10749e3068e
Tamilnila <tamilnilag@gmail.com>
2025-09-17 10:55:39 +0530

    corse error cleared

--- backend/src/main/java/com/aiinterviewpro/Controller/DepartmentManagementController.java
+++ backend/src/main/java/com/aiinterviewpro/Controller/DepartmentManagementController.java
@@ -7,15 +7,12 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.security.Principal;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/ca/addDepartment")
 
